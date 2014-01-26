ed8c4efcd7b4d4c48897cd94d5572cdbfcd289d2
Benjamin Marte <benmarte@gmail.com>
2014-01-26 14:56:00 -0500

    Added bootstrap-override and dehash_sprite
    Added the ability to override Bootstrap default variables by using the
    placeing the variables to override in _bootstrap-override.scss.
    
    Added the dehash_sprite setting to the config.rb in order to
    enable/disable it.

--- config.rb
+++ config.rb
@@ -23,8 +23,10 @@ line_comments         = false
  
 # The output style for the compiled css.  One of: `:nested`, `:expanded`,
 # `:compact`, or`:compressed`.
-output_style          = :compact
+output_style          = :compressed
 
+#Enable dehasher to remove generated sprite unique hash. Generates an image without the hash and uses it as the source sprite in the css
+dehash_sprite 				  = true
  
 # ==============================================================================
 # COMPASS SOURCE DIRECTORY CONFIGURATION
@@ -71,24 +73,25 @@ http_images_path      = http_path + images_dir
 # REMOVE CACHE HASH BUSTER FROM FILENAME
 # ==============================================================================
 
+if dehash_sprite == true
 # Make a copy of sprites with a name that has no uniqueness of the hash.
-on_sprite_saved do |filename|
-  if File.exists?(filename)
-    FileUtils.cp filename, filename.gsub(%r{-s[a-z0-9]{10}\.png$}, '.png')
-  end
-end
+	on_sprite_saved do |filename|
+	  if File.exists?(filename)
+	    FileUtils.cp filename, filename.gsub(%r{-s[a-z0-9]{10}\.png$}, '.png')
+	  end
+	end
 
-# Replace in stylesheets generated references to sprites
-# by their counterparts without the hash uniqueness.
-on_stylesheet_saved do |filename|
-  if File.exists?(filename)
-    css = File.read filename
-    File.open(filename, 'w+') do |f|
-      f << css.gsub(%r{-s[a-z0-9]{10}\.png}, '.png')
-    end
-  end
+	# Replace in stylesheets generated references to sprites
+	# by their counterparts without the hash uniqueness.
+	on_stylesheet_saved do |filename|
+	  if File.exists?(filename)
+	    css = File.read filename
+	    File.open(filename, 'w+') do |f|
+	      f << css.gsub(%r{-s[a-z0-9]{10}\.png}, '.png')
+	    end
+	  end
+	end
 end
-
 # ==============================================================================
 # THE END
 # ==============================================================================
